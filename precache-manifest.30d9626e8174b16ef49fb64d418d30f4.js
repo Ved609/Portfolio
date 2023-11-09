4d82344e68d46268866dc0f66bf8ac540463d93c
Ved609 <vedpatel4122004@gmail.com>
2023-11-09 23:20:22 +0530

    Updates

---
+++ precache-manifest.30d9626e8174b16ef49fb64d418d30f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528f14491a01d2a91d4335ba65b7c1a5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5ee05949fb9bc409f943",
+    "url": "/Portfolio/static/css/main.ad870a59.chunk.css"
+  },
+  {
+    "revision": "1fae3d92641b26ab1db7",
+    "url": "/Portfolio/static/js/2.329c713b.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/Portfolio/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "5ee05949fb9bc409f943",
+    "url": "/Portfolio/static/js/main.c00d4656.chunk.js"
+  },
+  {
+    "revision": "91a146410ea4bb6dee85",
+    "url": "/Portfolio/static/js/runtime~main.1ee3f3bf.js"
+  },
+  {
+    "revision": "03a4a5f034bf0bafa661fd8a8aabedc8",
+    "url": "/Portfolio/static/media/code.03a4a5f0.gif"
+  },
+  {
+    "revision": "b35215bb3be3ed577c5cea7d96d033c1",
+    "url": "/Portfolio/static/media/dbfe1f493ad01117fa4ec5ba10150e4d-computer-programming-logo.b35215bb.png"
+  }
+]);
